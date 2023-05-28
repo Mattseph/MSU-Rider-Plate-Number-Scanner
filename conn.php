0de4f74996d65110ead7d0e9039d2cd7a39990c2
Matthew Joseph Fang Bilaos <bilaos02311@gmail.com>
2023-05-28 21:56:26 +0800

    Edit: mysqli to mysql

--- conn.php
+++ conn.php
@@ -1,15 +1,14 @@
-<?php 
+<?php
 
 $host = "localhost:3308";
 $db_name = "msuriders";
 $username = "root";
-$password = "Bilaosrrmmmjg02311";
+$password = "Bilaosrrmmmjg02311_";
 
+$dsn = "mysql:host=$host;db_name=$db_name;charset=UTF8;";
 try {
-    $dsn = "mysqli:host=$host;db_name=$db_name;charset=UTF8;";
     $pdo = new PDO($dsn, $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
 } catch (PDOException $e) {
     echo $e->getMessage();
 }
